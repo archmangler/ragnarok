7878044b083b1aa2234e90c32748b88135f44086
Traiano Welcome <traiano@gmail.com>
2022-01-12 13:10:48 +0800

    basic redis test cluster

---
+++ microservices/storage/redis-storage/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#convenient helm insall from Azure Marketplace
+helm repo add azure-marketplace https://marketplace.azurecr.io/helm/v1/repo
+helm install my-release azure-marketplace/redis
