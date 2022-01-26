71c6deb17473db6981854584071eadeb68ec2799
Traiano Welcome <traiano@gmail.com>
2022-01-26 16:01:56 +0800

    use confluent kafka only

--- kafka/uninstall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-helm uninstall kafka incubator/kafka --namespace kafka
-watch kubectl get pods -n kafka
--- kafka/update.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#update/refresh settings from values.yaml
-helm upgrade kafka incubator/kafka -f values.yaml --namespace kafka
