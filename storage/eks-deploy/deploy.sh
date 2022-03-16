c019e14a121556d424b641701320a0dd94ac71dc
Traiano Giuseppe Welcome <traiano.welcome@accenture.com>
2022-03-16 12:49:08 +0800

    eks-s3 access service account

--- storage/eks-deploy/deploy.sh
+++ storage/eks-deploy/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+namespace="ragnarok"
+POLICY_ARN="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 
 function s3_deploy () {
  printf "initializing S3 terraform state ..."
@@ -7,4 +9,11 @@ function s3_deploy () {
  terraform apply terraform.plan
 }
 
-s3_deploy
+function create_eks_storage_access() {
+  eksctl utils associate-iam-oidc-provider --cluster=$AWS_CLUSTER_NAME --approve
+  eksctl delete iamserviceaccount --cluster=$AWS_CLUSTER_NAME --name=eks-s3-access --namespace=$namespace --approve
+  eksctl create iamserviceaccount --cluster=$AWS_CLUSTER_NAME --name=eks-s3-access --namespace=$namespace --attach-policy-arn="$POLICY_ARN" --approve
+}
+
+create_eks_storage_access
+#s3_deploy
--- storage/eks-deploy/eksS3access.sh
+++ storage/eks-deploy/eksS3access.sh
@@ -3,4 +3,5 @@
 namespace="ragnarok"
 POLICY_ARN="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 eksctl utils associate-iam-oidc-provider --cluster=$AWS_CLUSTER_NAME --approve
+eksctl delete iamserviceaccount --cluster=$AWS_CLUSTER_NAME --name=eks-s3-access --namespace=$namespace --approve
 eksctl create iamserviceaccount --cluster=$AWS_CLUSTER_NAME --name=eks-s3-access --namespace=$namespace --attach-policy-arn="$POLICY_ARN" --approve
